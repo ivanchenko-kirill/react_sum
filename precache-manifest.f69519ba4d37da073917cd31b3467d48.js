d1fbe4ada556eaf2f43057c9e8d646a60d2ae58b
Kirill Ivanchenko <ivanchenkok557@gmail.com>
2021-08-22 10:46:55 +0300

    Updates

---
+++ precache-manifest.f69519ba4d37da073917cd31b3467d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92095b50c2dd86423106d8ccb5327942",
+    "url": "./index.html"
+  },
+  {
+    "revision": "922bab6e98cb094e7ac0",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9cb276670027b79266c7",
+    "url": "./static/js/2.bafbf7af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bafbf7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "922bab6e98cb094e7ac0",
+    "url": "./static/js/main.feb67d91.chunk.js"
+  },
+  {
+    "revision": "c2acbe53827daed63a5f",
+    "url": "./static/js/runtime-main.1aec85e4.js"
+  }
+]);
